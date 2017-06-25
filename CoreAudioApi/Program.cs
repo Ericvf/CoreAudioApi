f57b44c3f7eb6dc1a5b1dccff2b6dd209871b267
Fex <e.feggelen@outlook.com>
2017-06-26 01:08:09 +0200

    Add project files.

---
+++ CoreAudioApi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace CoreAudioApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainTask = Task.Run(async () => await MainAsync());
+            mainTask.Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            await CoreAudioApi.MuteProcess("chrome", volumeStep: 0.05f, millisDelay: 50);
+        }
+    }
+}
